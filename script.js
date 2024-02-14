20463bb7df805c259edaf6762f0d8b8efce04a91
Baran Gülmüş <149194958+barangulmus@users.noreply.github.com>
2024-02-14 23:31:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const expand_btn = document.querySelector(".expand-btn");
+const img = expand_btn.querySelector("heade img");
+expand_btn.addEventListener("click", () => {
+  document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+
+const allLinks = document.querySelectorAll(".sidebar-links a");
+
+allLinks.forEach((elem) => {
+  elem.addEventListener('click', function() {
+    const hrefLinkClick = elem.href;
+
+    allLinks.forEach((link) => {
+      if (link.href == hrefLinkClick){
+        link.classList.add("active");
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  })
+});
+
+const mainItems = document.querySelectorAll(
+  '.main-item'
+);
+
+mainItems.forEach((mainItem) => {
+  mainItem.addEventListener('click', () => {
+    mainItem.classList.toggle(
+      'main-item--open'
+    );
+  })
+});
+iframe = document.getElementById("iframeId").src = "blog-preview-01/index.html";
