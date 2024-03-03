b148a487b93657c5acd08389a3483ce2ac953758
Baran Gülmüş <149194958+barangulmus@users.noreply.github.com>
2024-03-03 09:45:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const current = window.location.href;
 const allLinks = document.querySelectorAll(".sidebar-links tooltip");
 
 allLinks.forEach((elem) => {
-  elem.addEventListener('click', function() {
+  elem.addEventListener('click', function () {
     const hrefLinkClick = elem.href;
 
     allLinks.forEach((link) => {
-      if (link.href == hrefLinkClick){
+      if (link.href == hrefLinkClick) {
         link.classList.add("active");
       } else {
         link.classList.remove('active');
@@ -34,28 +34,28 @@ mainItems.forEach((mainItem) => {
     );
   })
 });
-document.getElementById("downloadLink").addEventListener("click", function() {
-    this.href=downloadXpath;
+document.getElementById("downloadLink").addEventListener("click", function () {
+  this.href = downloadXpath;
 });
 
 
 /*////////////////////////////////////////////////////////////////////////////////*/
 /*////////////////////////////////////////////////////////////////////////////////*/
 /*////////////////////////////////////////////////////////////////////////////////*/
-document.getElementById("closeforms").addEventListener("click", function() {
+document.getElementById("closeforms").addEventListener("click", function () {
   // document.getElementById("positionMiddleDiv").style.opacity = "0";
   // document.getElementById("positionMiddleDiv").style.pointerEvents = "none";
   document.getElementById("positionMiddleDiv").style = "opacity: 0; pointer-events:none;";
 });
 /*////////////////////////////////////////////////////////////////////////////////*/
 /*////////////////////////////////////////////////////////////////////////////////*/
-document.getElementById("addFileCard").addEventListener("click", function() {
+document.getElementById("addFileCard").addEventListener("click", function () {
   // document.getElementById("positionMiddleDiv").classList.add("animate__animated animate__bounceIn");
   document.getElementById("positionMiddleDiv").style = "opacity: 90%; pointer-events:all;";
 });
 /*////////////////////////////////////////////////////////////////////////////////*/
 /*////////////////////////////////////////////////////////////////////////////////*/
-document.getElementById("channeluploadButton").addEventListener("click", function() {
+document.getElementById("channeluploadButton").addEventListener("click", function () {
   document.getElementById("positionMiddleDiv").style = "opacity: 0; pointer-events:none;";
 });
 /*////////////////////////////////////////////////////////////////////////////////*/
@@ -115,11 +115,75 @@ const updateDropzoneFileList = (dropzoneElement, file) => {
     `;
 };
 
-// İptal butonuna tıklanınca tetiklenecek olan fonksiyon
-dropzoneBox.addEventListener("reset", (e) => {
-  let dropzoneFileMessage = dropZoneElement.querySelector(".message");
-
-  dropzoneFileMessage.innerHTML = `No Files Selected`;
-});
-
 // Kaydet butonuna tıklanınca tetiklenecek olan fonksiyon
+document.getElementById("submit-button").addEventListener("click", function () {
+  let fileName, xzy;
+  let fileNameLet = document.getElementById("filenameID");
+  let fileXpathLet = document.getElementById("openPageFileName");
+  // Radio button elementlerini al
+  const ButtonRadioButton = document.getElementById('a');
+  const CardRadioButton = document.getElementById('b');
+  const AccordionRadioButton = document.getElementById('c');
+  const AnimationRadioButton = document.getElementById('d');
+  const DropdownRadioButton = document.getElementById('e');
+  const FooterRadioButton = document.getElementById('f');
+
+  // Radio button'ların checked durumunu kontrol et
+  const ButtonChecked = ButtonRadioButton.checked;
+  const CardChecked = CardRadioButton.checked;
+  const AccordionChecked = AccordionRadioButton.checked;
+  const AnimationChecked = AnimationRadioButton.checked;
+  const DropdownChecked = DropdownRadioButton.checked;
+  const FooterChecked = FooterRadioButton.checked;
+
+  // Duruma göre işlem yap
+  if (ButtonChecked) {
+    fileName = document.querySelector("li#ButtonsLi div ul");
+    xzy = "buttons"
+  } else if (CardChecked) {
+    fileName = document.querySelector("li#CardsLi div ul");
+    xzy = "cards"
+  } else if (AccordionChecked) {
+    fileName = document.querySelector("li#AccordionsLi div ul");
+    xzy = "accordions"
+  } else if (AnimationChecked) {
+    fileName = document.querySelector("li#AnimationsLi div ul");
+    xzy = "animations"
+  } else if (DropdownChecked) {
+    fileName = document.querySelector("li#DropdownsLi div ul");
+    xzy = "dropdowns"
+  } else if (FooterChecked) {
+    fileName = document.querySelector("li#FootersLi div ul");
+    xzy = "footers"
+  } else {
+    alert("Lütfenn boş alan bırakmadığınızdan ve türkce karakter kullanmadıgınızdan emin olunuz.")
+  }
+// 'message' class'ına sahip tüm elementleri seç
+var elementler = document.getElementsByClassName("message");
+
+// İlk elemanı al (varsayılan olarak)
+var metin = elementler[0].textContent;
+
+// İlk elemanın metninde '.zip' olan son indeksi al
+var sonIndex = metin.indexOf('.zip');
+
+// 'class="message">' ifadesini aramaya başlamak için başlangıç indexi
+var baslangicIndex = metin.lastIndexOf('>', sonIndex) + 1;
+
+// Başlangıç indexinden son indexe kadar olan kısmı al, yani 'x.zip' kısmını.
+var zipDosyasiAdi = metin.substring(baslangicIndex, sonIndex).trim();
+
+console.log(zipDosyasiAdi);
+
+  dosyayolu = 
+  `<li>
+      <div class="line-vertical"></div>
+      <div class="line-horizontal"></div>
+      <a href="javascript:void(0)"
+        onclick="document.getElementById('iframeId').src = 'archive/${xzy}/${fileNameLet.value}/${zipDosyasiAdi}/${fileXpathLet.value}';
+        downloadXpath='downloadFolder/${zipDosyasiAdi}.zip'">
+        ${fileNameLet.value}
+      </a>
+    </li>`;
+  fileName.insertAdjacentHTML("beforeend", dosyayolu);
+});
